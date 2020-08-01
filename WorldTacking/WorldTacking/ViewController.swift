9adf6d5777c4bfe1d97d0165647ac5431a53dd4f
Mahima Srivastava <mahima5srivastava@gmail.com>
2020-08-01 11:37:35 +0530

    World Tracking

---
+++ WorldTacking/WorldTacking/ViewController.swift
@@ -0,0 +1,40 @@
+//
+//  ViewController.swift
+//  WorldTacking
+//
+//  Created by MAHIMA on 01/08/20.
+//  Copyright © 2020 MAHIMA. All rights reserved.
+//
+
+import UIKit
+import ARKit
+
+class ViewController: UIViewController {
+    
+    //MARK:- IBOutlets
+    
+    @IBOutlet weak var sceneView: ARSCNView!
+    
+    //MARK:- Properties
+    
+    let configuration = ARWorldTrackingConfiguration()
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        self.sceneView.debugOptions = [ARSCNDebugOptions.showWorldOrigin, ARSCNDebugOptions.showFeaturePoints]
+        self.sceneView.session.run(configuration)
+    }
+
+    //MARK:- IBActions
+    
+    @IBAction func addTapped(_ sender: Any) {
+        let node = SCNNode()
+        node.geometry = SCNBox(width: 0.1, height: 0.1, length: 0.1, chamferRadius: 0.8)
+        node.geometry?.firstMaterial?.diffuse.contents = UIColor.blue
+        node.position = SCNVector3(0, 0, -0.3)
+        sceneView.scene.rootNode.addChildNode(node)
+        
+    }
+    
+}
+
